e9809f0c30ffed608555fcdc16eda9090c143259
Natália Rodrigues <nataliamrq@gmail.com>
2021-08-25 23:35:29 -0300

    Modifying project configurations.

--- cyber_stock/cyber_stock/settings.py
+++ cyber_stock/cyber_stock/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zps+0^28-&#&51*&+!v&xy94(jki57t8twv=ez3x!^hfqb(mn@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
